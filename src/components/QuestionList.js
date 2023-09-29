ce631e44cb679960b0c73e0185c891de4954bb8f
Yer Yang <yyhmg_86@yahoo.com>
2023-09-28 23:41:38 -0700

    fetches a get request to the questions resource and display them

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,16 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions }) {
+  const listOfQuestion = questions.map((question) => (
+    <QuestionItem key={question.id} question={question} />
+  ))
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      {/* display QuestionItem components in the <ul> element after fetching */}
+      <ul>{listOfQuestion}</ul>
     </section>
   );
 }
